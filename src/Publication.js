7853908daaaf650f441fd0dc7e5bfbc691de5e52
ikkoham <kou.1515@gmail.com>
2017-11-09 23:55:21 +0900

    add arXiv:1709.07987

--- src/Publication.js
+++ src/Publication.js
@@ -4,16 +4,21 @@ class Publication extends Component {
   render() {
     return (
         <section id="publication">
-        <h2>
-        <a href="#publication"><i className="fa fa-book"></i>Publication</a>
-        </h2>
-        <ol reversed="reversed">
-        <li>
-        Relation between state-distinction power and disturbance in quantum measurements<br />
-        I. Hamamura, T. Miyadera<br />
-        arXiv: 1610.08814
-      </li>
-        </ol>
+          <h2>
+            <a href="#publication"><i className="fa fa-book"></i>Publication</a>
+          </h2>
+          <ol reversed="reversed">
+            <li>
+              Separability Criterion for Quantum Effects<br />
+              I. Hamamura<br />
+              arXiv:1709.07987
+            </li>
+            <li>
+              Relation between state-distinction power and disturbance in quantum measurements<br />
+              I. Hamamura, T. Miyadera<br />
+              arXiv:1610.08814
+            </li>
+          </ol>
         </section>
     );
   }
